5e0d90f99b458f6e1a91aaa334069cc02c4b54a6
Ryan Smith <ryan_smith2@hotmail.com>
2020-06-01 11:37:35 -0500

    added remaining info to get person in in function displayPerson

--- app.js
+++ app.js
@@ -94,6 +94,12 @@ function displayPerson(person){
   let personInfo = "First Name: " + person.firstName + "\n";
   personInfo += "Last Name: " + person.lastName + "\n";
   // TODO: finish getting the rest of the information to display
+  personInfo += "Gender: " + person.gender + "\n";
+  personInfo += "DOB: " + person.dob + "\n";
+  personInfo += "Height" + person.height + "\n";
+  personInfo += "Weight" + person.weight + "\n";
+  personInfo += "Eye Color" + person.eyeColor + "\n";
+  personInfo += "Occupation" + person.occupation + "\n";
   alert(personInfo);
 }
 
